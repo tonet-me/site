6199acab2b1436126d275b5a9e1efa961d228ccc
hamidreza <mohtashami911@gmail.com>
2024-05-09 20:29:33 +0400

    tonet

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideClientHydration} from '@angular/platform-browser';
-import {provideHttpClient, withInterceptors} from "@angular/common/http";
+import {provideHttpClient, withFetch, withInterceptors} from "@angular/common/http";
 import {baseUrlInterceptor} from "@core/http/base-url.interceptor";
 import {provideAnimations} from "@angular/platform-browser/animations";
 import {provideToastr} from "ngx-toastr";
@@ -12,7 +12,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withInterceptors([
+    provideHttpClient(withFetch(), withInterceptors([
       baseUrlInterceptor,
     ])),
     provideAnimations(), // required animations providers
